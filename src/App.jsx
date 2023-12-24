95adfee7bbfd424b39241bcc5597792de8c862e9
Praveen Kumar Padhi <93372387+praveenkupadhi@users.noreply.github.com>
2023-12-24 11:33:45 +0530

    prettier format

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,61 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-	const [count, setCount] = useState(1);
-	const [maxCount, setMaxCount] = useState(1000);
+  const [count, setCount] = useState(1);
+  const [maxCount, setMaxCount] = useState(1000);
 
-	const handleCountSub = () => {
-		if (count > 1) {
-			setCount(count - 1);
-		}
-	};
+  const handleCountSub = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
 
-	const handleCountAdd = () => {
-		if (count < maxCount) {
-			if (typeof count === "string") setCount(+count + 1);
-			else setCount(count + 1);
-		}
-	};
+  const handleCountAdd = () => {
+    if (count < maxCount) {
+      if (typeof count === 'string') setCount(+count + 1);
+      else setCount(count + 1);
+    }
+  };
 
-	return (
-		<div className="App">
-			<div id="input-value">
-				<input
-					type="number"
-					placeholder="Enter initial count"
-					onChange={(e) => {
-						setCount(+e.target.value);
-					}}
-				/>
-				<input
-					type="number"
-					placeholder="Enter max count"
-					onChange={(e) => {
-						if (e.target.value === "") setMaxCount(1000);
-						else setMaxCount(+e.target.value);
-					}}
-				/>
-			</div>
-			<div id="counter">
-				<button onClick={handleCountSub} id="dec">
-					-
-				</button>
-				<input
-					type="number"
-					value={count}
-					onChange={(e) => {
-						if (e.target.value === "") setCount("");
-						if (+e.target.value > 0 && +e.target.value <= 1000)
-							setCount(+e.target.value);
-					}}
-				/>
-				<button onClick={handleCountAdd} id="inc">
-					+
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div id="input-value">
+        <input
+          type="number"
+          placeholder="Enter initial count"
+          onChange={(e) => {
+            setCount(+e.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Enter max count"
+          onChange={(e) => {
+            if (e.target.value === '') setMaxCount(1000);
+            else setMaxCount(+e.target.value);
+          }}
+        />
+      </div>
+      <div id="counter">
+        <button onClick={handleCountSub} id="dec">
+          -
+        </button>
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => {
+            if (e.target.value === '') setCount('');
+            if (+e.target.value > 0 && +e.target.value <= 1000)
+              setCount(+e.target.value);
+          }}
+        />
+        <button onClick={handleCountAdd} id="inc">
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
